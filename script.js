08a9b0d656d8ecb15a21749fb1ebc7fdb3b8e22b
Bitu kumar <bituku347@gmail.com>
2024-03-28 11:43:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+const hour = document.querySelector('.hour');
+
+setInterval(()=>{
+    const date = new Date;
+    const secDeg = date.getSeconds() / 60 * 360 -90;
+    const minDeg = date.getMinutes() / 60 * 360 -90;
+    const hourDeg = date.getHours() / 12 * 360 -90;
+    sec.style.transform = `rotate(${secDeg}deg)`;
+    min.style.transform = `rotate(${minDeg}deg)`;
+    hour.style.transform = `rotate(${hourDeg}deg)`;
+},1000)
